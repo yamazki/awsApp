cccc16c9c11c92db94bda1b79d098cdfc207ef2a
EC2 Default User <ec2-user@ip-172-31-7-232.ap-northeast-1.compute.internal>
2018-02-17 04:29:23 +0000

    task3 finished

--- node/index.js
+++ node/index.js
@@ -1,20 +1,33 @@
 const app = require("express")();
-const http = require('http').Server(app);
-const basicAuth = require('basic-auth-connect');
+const http = require("http").Server(app);
+const basicAuth = require("basic-auth-connect");
+const eval = require("eval")
 
 //Task1
-app.get('/', function(req, res){
-  res.send('AMAZON');
+app.get("/", function(req, res){
+  res.send("AMAZON");
 });
 
 //Task2
-app.all('/secret', basicAuth(function(user, password) {
-    return user === 'amazon' && password === 'candidate';
+app.all("/secret", basicAuth(function(user, password) {
+    return user === "amazon" && password === "candidate";
 }));
 
-app.get('/secret', function(req, res){
-  res.send('SUCCESS');
+app.get("/secret", function(req, res){
+  res.send("SUCCESS");
 });
+
+//Task3
+app.get("/calc", function(req, res){
+  try {
+    let s = req.url.split("?");
+    let caliculateResult = eval("let result = " + s[1] + "; exports.result = result")
+    res.send(String(caliculateResult.result));
+  } catch (e) {
+    res.send("ERROR");
+  }
+});
+
 
 var server = app.listen(80, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
